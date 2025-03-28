53bdd3f5fc4a618ce68279a52e5a16e723b9828e
Stetik <132297315+Stetik@users.noreply.github.com>
2025-03-28 15:46:10 -0300

    Added UI + minor changes

--- Assets/ActivateTeleportationRay.cs
+++ Assets/ActivateTeleportationRay.cs
@@ -11,8 +11,12 @@ public class ActivateTeleportationRay : MonoBehaviour
     public InputActionProperty rightActivate;
 
     public InputActionProperty rightCancel;
+
+    public XRRayInteractor rightRay;
     void Update()
     {
-        rightTeleportation.SetActive(rightCancel.action.ReadValue<float>() == 0 && rightActivate.action.ReadValue<float>() > 0.1f);
+        bool isRightRayHovering = rightRay.TryGetHitInfo(out Vector3 rightPos, out Vector3 rightNormal, out int rightNumber, out bool rightValid);
+
+        rightTeleportation.SetActive(!isRightRayHovering && rightCancel.action.ReadValue<float>() == 0 && rightActivate.action.ReadValue<float>() > 0.1f);
     }
 }
